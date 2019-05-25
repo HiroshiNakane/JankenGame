f5f17d1df07c49e677497a7e3a8e4ef5f84dac5a
中根寛志 <tsmus3105@gmail.com>
2019-05-25 18:52:41 +0900

    ファイルの作成と、じゃんけん処理の準備

---
+++ Assets/Sprites/Janken.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class janken : MonoBehaviour
+{
+
+    //Image jankenHand;
+
+    /*public Sprite choki;
+    public Sprite gu;
+    public Sprite pa;*/
+
+    void Start()
+    {
+        // 対象を探す
+        var button = GameObject.Find("StartButton").GetComponent<Button>();
+        var chokiButton = GameObject.Find("choki").GetComponent<Button>();
+        var guButton = GameObject.Find("gu").GetComponent<Button>();
+        var paButton = GameObject.Find("pa").GetComponent<Button>();
+        //jankenHand = GameObject.Find("handImage").GetComponent<Image>();
+
+    }
+
+    void Update()
+    {
+
+    }
+}
